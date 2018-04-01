cbbfbfcbebe2095dfc0635b89b87512cf2076ddf
Tobias Koch <t.koch@tk-software.de>
2018-04-01 20:11:56 +0200

    Adding set and get methods

--- ScreenSaver/ScreenSaver/IO/Configuration.cs
+++ ScreenSaver/ScreenSaver/IO/Configuration.cs
@@ -27,6 +27,7 @@
 
 #region Namespaces
 using System;
+using System.Collections.Generic;
 #endregion
 
 namespace ScreenSaver.IO
@@ -36,5 +37,141 @@ namespace ScreenSaver.IO
     /// </summary>
     public class Configuration
     {
+        #region Constants and Fields
+
+        /// <summary>
+        /// Contains keys and values.
+        /// </summary>
+        private Dictionary<string, string> values;
+
+        /// <summary>
+        /// Represents the lock object used to synchronize access to the <see cref="values"/> dictionary.
+        /// </summary>
+        private object valuesLock = new object();
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Configuration"/> class.
+        /// </summary>
+        public Configuration()
+        {
+            this.values = new Dictionary<string, string>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Sets the given key to the given value.
+        /// </summary>
+        /// <param name="key">The key that shall be used to store the value.</param>
+        /// <param name="value">The value that shall be stored.</param>
+        /// <exception cref="ArgumentNullException"><c>key</c> is null.</exception>
+        /// <exception cref="ArgumentNullException"><c>value</c> is null.</exception>
+        /// <exception cref="ArgumentException"><c>key</c> is empty.</exception>
+        public void SetValue(string key, string value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (key.Trim() == string.Empty)
+            {
+                throw new ArgumentException("key is empty", nameof(key));
+            }
+
+            key = key.ToLower();
+
+            lock (this.valuesLock)
+            {
+                if (this.values.ContainsKey(key))
+                {
+                    this.values[key] = value;
+                }
+                else
+                {
+                    this.values.Add(key, value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the value associated with the given key.
+        /// </summary>
+        /// <param name="key">The key that shall be used to retrieve the value.</param>
+        /// <returns>The value associated with the given key.</returns>
+        /// <exception cref="ArgumentNullException"><c>key</c> is null.</exception>
+        /// <exception cref="ArgumentException"><c>key</c> is empty.</exception>
+        /// <exception cref="ArgumentException"><c>key</c> does not contain a value.</exception>
+        public string GetValue(string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (key.Trim() == string.Empty)
+            {
+                throw new ArgumentException("key is empty", nameof(key));
+            }
+
+            key = key.ToLower();
+
+            lock (this.valuesLock)
+            {
+                if (!this.values.ContainsKey(key))
+                {
+                    throw new ArgumentException("key not found", nameof(key));
+                }
+
+                return this.values[key];
+            }
+        }
+
+        /// <summary>
+        /// Gets the value associated with the given key or the given default value if there is no value stored for the key..
+        /// </summary>
+        /// <param name="key">The key that shall be used to retrieve the value.</param>
+        /// <param name="defaultValue">The value that shall be returned if there is no stored value for the given key.</param>
+        /// <returns>The value associated with the given key or the default value.</returns>
+        /// <exception cref="ArgumentNullException"><c>key</c> is null.</exception>
+        /// <exception cref="ArgumentException"><c>key</c> is empty.</exception>
+        public string GetValue(string key, string defaultValue)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (key.Trim() == string.Empty)
+            {
+                throw new ArgumentException("key is empty", nameof(key));
+            }
+
+            try
+            {
+                return this.GetValue(key);
+            }
+            catch (ArgumentException)
+            {
+                return defaultValue;
+            }
+        }
+
+        #endregion
     }
 }
