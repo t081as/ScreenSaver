f3eb3317d06bd09e9bb2b3846280d3dd46c45306
Tobias Koch <t.koch@tk-software.de>
2018-04-05 20:25:14 +0200

    Implementing slide show class

--- ScreenSaver/ScreenSaver/Media/SlideShow.cs
+++ ScreenSaver/ScreenSaver/Media/SlideShow.cs
@@ -27,6 +27,7 @@
 
 #region Namespaces
 using System;
+using System.Threading;
 #endregion
 
 namespace ScreenSaver.Media
@@ -36,5 +37,113 @@ namespace ScreenSaver.Media
     /// </summary>
     public class SlideShow
     {
+        #region Constants and Fields
+
+        /// <summary>
+        /// An implementation of the <see cref="ISlideShowConfiguration"/> interface.
+        /// </summary>
+        private ISlideShowConfiguration configuration;
+
+        /// <summary>
+        /// Reference to the thread rendering the slideshow.
+        /// </summary>
+        private Thread slidesRenderingThread;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SlideShow"/> class.
+        /// </summary>
+        /// <param name="configuration">An implementation of the <see cref="ISlideShowConfiguration"/> interface.</param>
+        /// <exception cref="ArgumentNullException"><c>configuration</c> is null.</exception>
+        public SlideShow(ISlideShowConfiguration configuration)
+        {
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            this.configuration = configuration;
+            this.slidesRenderingThread = null;
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Triggered when a new image of the slideshow has been rendered.
+        /// </summary>
+        public event EventHandler ImageRendered;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Starts the Slideshow.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The slideshow has already been started.</exception>
+        public void Start()
+        {
+            if (this.slidesRenderingThread != null)
+            {
+                throw new InvalidOperationException("Slideshow has already been started");
+            }
+
+            this.slidesRenderingThread = new Thread(new ThreadStart(this.SlideRendering));
+            this.slidesRenderingThread.IsBackground = true;
+            this.slidesRenderingThread.Name = "Slideshow Rendering";
+
+            this.slidesRenderingThread.Start();
+        }
+
+        /// <summary>
+        /// Stops the slideshow.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The slideshow has not been started.</exception>
+        public void Stop()
+        {
+            if (this.slidesRenderingThread == null)
+            {
+                throw new InvalidOperationException("Slideshow has not been started");
+            }
+
+            this.slidesRenderingThread.Abort();
+            this.slidesRenderingThread = null;
+        }
+
+        /// <summary>
+        /// Rendering method executed in a background thread.
+        /// </summary>
+        protected virtual void SlideRendering()
+        {
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+
+                    }
+                    catch (ThreadAbortException)
+                    {
+                        throw;
+                    }
+                    catch (Exception)
+                    {
+                        // Ignore exceptions
+                    }
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                Thread.ResetAbort();
+            }
+        }
+
+        #endregion
     }
 }
